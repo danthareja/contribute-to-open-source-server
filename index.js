abfeb796fe4943162da97e94ff86a81bfd7bd2c7
Dan Thareja <danthareja@gmail.com>
2018-01-30 17:35:06 -0700

    Add scheduled task for branch cleanup

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 module.exports.handleGitHubWebhook = require('./hooks/github');
 module.exports.handleCircleCIWebhook = require('./hooks/circleci');
+module.exports.handleCleanupTask = require('./tasks/cleanup');
