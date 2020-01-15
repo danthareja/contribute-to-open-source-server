4b573b15084ddaff5819bc4d37f5d2ab21a69dd2
Dan Thareja <danthareja@gmail.com>
2020-01-14 18:17:49 -0700

    Remove issue locking

--- src/hooks/github/events/issues.js
+++ src/hooks/github/events/issues.js
@@ -7,36 +7,25 @@ module.exports = async function issues({ issue, action }) {
     return;
   }
 
-  if (action !== 'opened' && action !== 'reopened') {
+  const allowedActions = ['opened', 'reopened'];
+
+  if (allowedActions.indexOf(action) === -1) {
     console.log(`Ignoring action: ${action}`);
     return;
   }
 
   if (action === 'opened') {
-    console.log(`Closing and locking issue #${issue.number}`);
+    console.log(`Closing opened issue #${issue.number}`);
     await github.post(`/issues/${issue.number}/comments`, {
       body: comments.issueOpen({ issue })
     });
-    await github.patch(`/issues/${issue.number}`, {
-      state: 'closed'
-    });
-    await github.put(`/issues/${issue.number}/lock`, null, {
-      headers: {
-        'Content-Length': 0
-      }
-    });
-    return {
-      success: true
-    };
   }
 
-  if (action === 'reopened') {
-    console.log(`Closing reopened issue #${issue.number}`);
-    await github.patch(`/issues/${issue.number}`, {
-      state: 'closed'
-    });
-    return {
-      success: true
-    };
-  }
+  await github.patch(`/issues/${issue.number}`, {
+    state: 'closed'
+  });
+
+  return {
+    success: true
+  };
 };
